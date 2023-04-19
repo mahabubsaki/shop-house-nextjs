51308be578d5677e3211d9975030df5fdc1aeebd
mahabubsaki <md929820@gmail.com>
2023-04-19 18:48:40 +0600

    combined express server

---
+++ backend/dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var app = (0, express_1.default)();
+var port = process.env.PORT || 6969;
+app.get('/', function (req, res) {
+    res.send('Express + TypeScript Servers');
+});
+app.listen(port, function () {
+    console.log("\u26A1\uFE0F[server]: Server is running at http://localhost:".concat(port));
+});
+//# sourceMappingURL=server.js.map
